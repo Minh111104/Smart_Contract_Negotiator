eea0220dba88de4fc0792d3bd12d0eb4cf12011b
Quang Minh <quangminh111104@gmail.com>
2025-07-02 22:59:21 +0700

    Set up Redux Store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import editorReducer from '../features/editor/editorSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    editor: editorReducer,
   },
 });
