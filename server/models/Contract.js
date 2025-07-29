b86e1475ecfa46731608bf3c26e79ea2040d3552
Quang Minh <quangminh111104@gmail.com>
2025-07-29 23:47:00 +0700

    feat: create contract model

--- server/models/Contract.js
+++ server/models/Contract.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
 
 const ContractSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, default: '' },
-  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  lastEdited: { type: Date, default: Date.now },
+  title: { 
+    type: String, 
+    required: true 
+  },
+  content: { 
+    type: String, 
+    default: '' 
+  },
+  participants: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User' 
+  }],
+  lastEdited: { 
+    type: Date, 
+    default: Date.now 
+  }
+}, {
+  timestamps: true
 });
 
 module.exports = mongoose.model('Contract', ContractSchema);
