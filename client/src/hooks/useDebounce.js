e627b0df3dd38ccd34c34fa68e3d6f1959713a6d
Quang Minh <quangminh111104@gmail.com>
2025-08-02 03:05:22 +0700

    feat: add Auto-save with Debouncing

---
+++ client/src/hooks/useDebounce.js
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from 'react';
+
+function useDebounce(callback, delay) {
+  const timeoutRef = useRef(null);
+
+  useEffect(() => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      callback();
+    }, delay);
+
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, [callback, delay]);
+}
+
+export default useDebounce; 
